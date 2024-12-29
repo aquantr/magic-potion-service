5576cb0dfb4d582d80e9de1d792deb54a0b9af4f
Anh Quan Truong <quanat1x4@gmail.com>
2024-12-29 17:32:57 -0500

    added a service class to allow interactions with database

--- src/main/java/com/magicpotion/magic_potion_service/service/PotionService.java
+++ src/main/java/com/magicpotion/magic_potion_service/service/PotionService.java
@@ -1,4 +1,33 @@
 package com.magicpotion.magic_potion_service.service;
 
+import com.magicpotion.magic_potion_service.model.Potion;
+import com.magicpotion.magic_potion_service.repository.PotionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class PotionService {
+
+    private static final Logger logger = LoggerFactory.getLogger(PotionService.class);
+
+    private final PotionRepository potionRepository;
+
+    public PotionService(PotionRepository potionRepository) {
+        this.potionRepository = potionRepository;
+    }
+
+    public List<Potion> getAllPotions() {
+        List<Potion> potions = potionRepository.findAll();
+        logger.info("Retrieved potions: {}", potions);
+        return potions;
+    }
+
+    public Potion addPotion(Potion potion) {
+        logger.info("Adding potion: {}", potion);
+        return potionRepository.save(potion);
+    }
+
 }
