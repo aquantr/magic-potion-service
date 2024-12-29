992b1c7447ecf7f8d41c3fe348e80b260b509d58
Anh Quan Truong <quanat1x4@gmail.com>
2024-12-29 14:42:42 -0500

    initial commit

---
+++ src/main/java/com/magicpotion/magic_potion_service/MagicPotionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.magicpotion.magic_potion_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicPotionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicPotionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magicpotion/magic_potion_service/MagicPotionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magicpotion.magic_potion_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicPotionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
