5385c70c627dc7087f9d83b41a61fa7d8ba6cb0a
Anh Quan Truong <quanat1x4@gmail.com>
2024-12-29 17:32:04 -0500

    added a repo for potions

--- src/main/java/com/magicpotion/magic_potion_service/repository/PotionRepository.java
+++ src/main/java/com/magicpotion/magic_potion_service/repository/PotionRepository.java
@@ -1,4 +1,9 @@
 package com.magicpotion.magic_potion_service.repository;
 
-public class PotionRepository {
+import com.magicpotion.magic_potion_service.model.Potion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PotionRepository extends JpaRepository<Potion, Long> {
 }
