9d9711a6855d84876f0ab381814c09e354946a89
Anh Quan Truong <quanat1x4@gmail.com>
2024-12-29 17:31:08 -0500

    added a potion entity

--- src/main/java/com/magicpotion/magic_potion_service/model/Potion.java
+++ src/main/java/com/magicpotion/magic_potion_service/model/Potion.java
@@ -1,4 +1,28 @@
 package com.magicpotion.magic_potion_service.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Potion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    @Override
+    public String toString() {
+        return "Potion{id=" + id + ", name='" + name + "', description='" + description + "'}";
+    }
 }
